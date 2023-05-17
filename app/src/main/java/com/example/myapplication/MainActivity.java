fb42f84bb6a55e0d2e4b6c9705a2bf0702ef081a
BKXC46 <shushrut.deshpande@motorolasolutions.com>
2023-05-17 15:33:56 +0530

    modified and commiting to the working branch

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -9,6 +9,11 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //Commiting on loacl and then raising pull request to merge to main branch
+        //so lets do some changes and push the changes to the main branch later
+        //So wel come to the world of learning the Git thing and pushing the changes.
+        //how do you feel learning the git and then working on it.
+        // Please do share the feedbacks so that we can improvise on the areas.
         setContentView(R.layout.activity_main);
     }
 }
